15be1bd0ceff7912900a460be37ea6cff6afabbb
Karlo Hlisc <karlohlisc.kh@gmail.com>
2026-02-04 17:34:14 +0100

    Add message to container element in script.js

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
- document.getElementById("title-first").innerText="Es funktionier wie es soll";
+document.getElementById("title-first").innerText="Es funktionier wie es soll";  
+
+document.getElementById("container").innerHTML="<p>Heute ist ein schöner Tag!</p>";
